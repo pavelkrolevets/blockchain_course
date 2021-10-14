61e9772a76dc12d54b4a55b19501e12e12ea47bb
p.krolevets@dltru.org <p.krolevets@fintechru.org>
2021-10-14 14:50:37 +0600

    update module 4

--- Module_4/front-end/src/App.js
+++ Module_4/front-end/src/App.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
+import './App.css';
 
-export default function App() {
+function App() {
   const [show_balance, set_balance] = useState(0);
-  const [transaction_message, set_transaction_message] = useState(
-    "Simple transaction message"
-  );
-  const [raw_transaction_message, set_raw_transaction_message] = useState(
-    "Signed transaction message"
-  );
+  const [show_number, set_number] = useState(0);
 
   function getBalance() {
     var Web3 = require("web3");
@@ -26,54 +22,72 @@ export default function App() {
       });
   }
 
-  function sendTransaction() {
-    var Web3 = require("web3");
-    var web3 = new Web3(
-      new Web3.providers.HttpProvider("http://localhost:8545")
-    );
+  function sendRawTransaction(){
+  var Web3 = require("web3");
+  const Tx = require("ethereumjs-tx");
 
-    console.log("Sending  transaction");
-
+  // Show Web3 where it needs to look for a connection to Ethereum.
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     web3.eth
-      .sendTransaction({
-        from: "0xA31c2434062757563A6E27379dDD512F827e8795",
-        to: "0x4faF226eA0437A14ae882Fc05Df2439029312E3E",
-        value: "1000000000000000000"
-      })
-      .on("transactionHash", function (hash) {
-        console.log("Hash", hash);
-        set_transaction_message("Transaction hash " + hash);
-      })
-      .on("receipt", function (receipt) {
-        console.log("Receipt ", receipt);
-      })
-      .on("confirmation", function (confirmationNumber, receipt) {
-        console.log("Confirmation ", parseInt(confirmationNumber, 10));
-        set_transaction_message("Confirmation " + confirmationNumber);
-        if (parseInt(confirmationNumber, 10)===6) {
-          set_transaction_message("Sucess! " + receipt.blockNumber);
-        }
+      .getTransactionCount("0xA31c2434062757563A6E27379dDD512F827e8795", "pending")
+      .then((nonce) => {
+        console.log("Nonce  : ", nonce);
+        var gasPrice = "2"; //or get with web3.eth.gasPrice
+        var gasLimit = 3000000;
+        var addr = "0xA31c2434062757563A6E27379dDD512F827e8795";
+        var toAddress = "0x4faF226eA0437A14ae882Fc05Df2439029312E3E";
+        var amountToSend = "1000000000000000000"; //1 ETH
 
-      })
-      .on("error", console.error);
+        var rawTransaction = {
+          from: addr,
+          nonce: web3.utils.toHex(nonce),
+          gasPrice: web3.utils.toHex(gasPrice * 1e9),
+          gasLimit: web3.utils.toHex(gasLimit),
+          to: toAddress,
+          value: web3.utils.toHex(amountToSend),
+          chainId: 50074, //remember to change this
+        };
+
+        var privateKey =
+          "750abe8e310507f4aaa0043d3bcea78885924c3761bcdd16e92991d9c3ac4b52";
+        var privKey = new Buffer(privateKey, "hex");
+
+        console.log("privKey  : ", privKey);
+
+        var tx = new Tx(rawTransaction);
+
+        tx.sign(privKey);
+
+        var serializedTx = tx.serialize();
+        console.log("serializedTx : " + serializedTx);
+
+        web3.eth.sendSignedTransaction(
+          "0x" + serializedTx.toString("hex"),
+          function (err, hash) {
+            if (!err) {
+              set_number(hash);
+              console.log("Txn Sent and hash is " + hash);
+            } else {
+              set_number(hash);
+              console.error(err);
+            }
+          }
+        );
+      });
   }
+
+    
+    
 
 
   return (
     <div className="App">
-      <div>
-        <h4>Hello world Web3js</h4>
-      </div>
-
-      <div>
-        <button onClick={getBalance}> Get Balance</button>
-        <h4>{show_balance}</h4>
-      </div>
-
-      <div>
-        <button onClick={sendTransaction}> Send simple transaction</button>
-        <h4>{transaction_message}</h4>
-      </div>
+      <button onClick={getBalance}>BALANCE</button>
+      <h2>{show_balance}</h2>
+      <button onClick={sendRawTransaction}>Send raw transaction</button>
+      <h2>{show_number}</h2>
     </div>
   );
 }
+
+export default App;
